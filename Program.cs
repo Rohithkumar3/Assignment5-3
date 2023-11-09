db6d2f6d22a7ed7524847727c48e3f6004317c56
Rohithkumar <rohithkumargande@gmail.com>
2023-11-09 15:43:24 +0530

    part3

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_part3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First 10 numbers in the Fibonacci sequence:");
+
+            int n = 10, f1 = 0, f2 = 1, f3;
+
+           
+                   
+            for (int i = 0; i < n; i++)
+            {
+                f3 = f1 + f2;
+                Console.WriteLine(f1);
+                f1 = f2;
+                f2 = f3;
+                Console.ReadKey();
+            }
+                }
+                
+            }
+        }
+    
+
+   
